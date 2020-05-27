dc015554b70b486c8e54d2396be3f45efedf1c60
ddiddws2 <34942293+ddiddws2@users.noreply.github.com>
2020-05-27 19:10:37 +0900

    third

--- py1.py
+++ py1.py
@@ -1,11 +1,11 @@
 import inspect
 import urllib.request
 
-x= str(input("これから画像を取得します。よろしいですか？　Y or N"))
+x= str(input("これから画像を取得します。\nよろしいですか？　Y or N : "))
 
 if x == "Y":
     url = "http://192.168.33.80/wp-content/uploads/2020/04/idl_OoC9_147_452-1-scaled.jpg"
-    name = "ikutanana.jpg"
+    name = "ikutaerika1.jpg"
     urllib.request.urlretrieve(url, name)
 else:
     pass
