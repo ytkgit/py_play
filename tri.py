f2f62bd881428d998371d32d33a64cf061e39191
YTK <ddiddws2@gmail.com>
2020-05-28 00:11:12 +0900

    fourth

--- tri.py
+++ tri.py
@@ -1,3 +1,5 @@
+####FUNC1 BEGIN###
+"""" simple math """
 #This is sample function to showcase power
 # import time
 # print ("This function shows the powered number you input")
@@ -16,8 +18,10 @@
 # y=str(input("Please input your favorite word:"))
 # print("word count ", len(y))
 # print(y,"...Nice!, I like it too")
+####FUNC1 END###
 
-
+####FUNC2 BEGIN###
+"""How  the math works"""
 # math = int(input("Please input Li\'s Math score:"))
 # jap = int(input("Please input Li\'s jap score:"))
 # science = int(input("Please input Li\'s science score:"))
@@ -32,7 +36,9 @@
 #         else:
 #             #loop fell through without finding a factor
 #             print(n,'is a prime number')
+####FUNC2 END###
 
+####FUNC2 OPTIONS###
 # #continue pattern
 # for num in range(2,10):
 #     if num %2 == 0:
@@ -57,23 +63,26 @@
 #     print()
 
 
+####FUNC3 BEGIN###
+"""This is downloading fuction which fetch the data from the specified URL"""
 # import urllib.request
-url = 'http://192.168.33.10/wp-content/uploads/2020/04/fa125c97f7b4281b2959095622154e18.jpg'
-name = 'yane1.jpg'
-#ask before the download
-ans = str(input("Do you want download the image?(input Yes or No): "))
-if ans == "Yes":
-    urllib.request.urlretrieve(url, name)
-    print('saved the image as ', name)
+# url = 'http://192.168.33.10/wp-content/uploads/2020/04/fa125c97f7b4281b2959095622154e18.jpg'
+# name = 'yane1.jpg'
+# #ask before the download
+# ans = str(input("Do you want download the image?(input Yes or No): "))
+# if ans == "Yes":
+#     urllib.request.urlretrieve(url, name)
+#     print('saved the image as ', name)
 
-elif ans == "No":
-    print("Donwload stopped")
+# elif ans == "No":
+#     print("Donwload stopped")
+####FUNC3 END###
 
-
-with open("test.txt",mode="x") as f:
-    for i in range(10):
-        x = i**i
-    print(x)
+####FUNC4 BEGIN###
+# with open("test.txt",mode="x") as f:
+#     for i in range(10):
+#         x = i**i
+#     print(x)
 
 
 # #get url request
@@ -85,4 +94,21 @@ with open("test.txt",mode="x") as f:
 
 # text = data.decode("utf-8")
 # print(text)
+####FUNC4 END###
 
+
+###FUNC5 BEGIN###
+"""Get the data from the url"""
+#import section
+import urllib.request
+import time
+
+#variable section
+url = "http://192.168.33.10/wp-content/uploads/2020/04/f0b3b5c08f768ef5eaf13c596f68ee34-547x1024.jpg"
+name = "hashimotokanna1.png"
+
+#control section
+print("Downloading the image will begin")
+time.sleep(2) 
+urllib.request.urlretrieve(url,name)
+print("Done")
