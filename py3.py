3e98b3313a84fe0efc6c3cb92afe2efec5f1c2ef
ddiddws2 <34942293+ddiddws2@users.noreply.github.com>
2020-05-30 21:45:15 +0900

    py3.py added

---
+++ py3.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+###MOD1 BEGIN###
+"""input the url and parse the html"""
+
+#1 import library
+from bs4 import BeautifulSoup
+import urllib.request as req
+import urllib.parse as par
+
+#1 variables
+url = str(input("解析したいURLを入力してください :"))
+
+#2 parse the html
+with req.urlopen(url) as r:
+    b = r.read()
+    data = b.decode("utf-8")
+
+soup = BeautifulSoup(data,'html.parser')
+
+#3 find all method
+links = soup.find_all("a")
+
+#4 show links
+for a in links:
+    href = a.attrs["href"]
+    text = a.string
+    print(text," > ",href)
+###MOD1 END###
